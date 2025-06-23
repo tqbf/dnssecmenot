b797e8c5d316f93963b4038683aa3d1eef959979
Thomas bH. Ptacek <thomas@sockpuppet.org>
2025-06-22 19:49:17 -0500

    clean this up a little, but -list-unclassed is busted (needs to check rank)

--- main.go
+++ main.go
@@ -29,37 +29,48 @@ var templates = template.Must(
 )
 
 func main() {
-	updatePath := flag.String("update-classes", "", "load classes")
-	listFlag := flag.Bool("list-unclassed", false, "list domains")
-	flag.Parse()
 	h := slog.NewTextHandler(os.Stderr, nil)
 	slog.SetDefault(slog.New(h))
+
+	var (
+		updatePath = flag.String("update-classes", "", "load classes")
+		listFlag   = flag.Bool("list-unclassed", false, "list domains")
+	)
+	flag.Parse()
+
 	db, err := openDB()
 	if err != nil {
 		slog.Error("open db", "err", err)
 		os.Exit(1)
 	}
 	defer db.Close()
+
 	if err := maybeSeedDomains(db); err != nil {
 		slog.Error("seed", "err", err)
 		os.Exit(1)
 	}
-	if *updatePath != "" {
+
+	switch {
+	case *updatePath != "":
 		if err := loadClasses(db, *updatePath); err != nil {
 			slog.Error("classes", "err", err)
 			os.Exit(1)
 		}
 		return
-	}
-	if *listFlag {
+
+	case *listFlag:
 		if err := listUnclassed(db, os.Stdout); err != nil {
 			slog.Error("list", "err", err)
 			os.Exit(1)
 		}
 		return
 	}
+
+	// nope we're servering
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
 	if err := startScheduler(ctx, db); err != nil {
 		slog.Error("scheduler", "err", err)
 		os.Exit(1)
@@ -105,17 +116,20 @@ func loadClasses(db *sql.DB, path string) error {
 	defer file.Close()
 	data, err := io.ReadAll(file)
 	if err != nil {
-		return err
+		return fmt.Errorf("read: %w", err)
 	}
+
 	var m map[string][]string
 	if err := json.Unmarshal(data, &m); err != nil {
 		return err
 	}
+
 	tx, err := db.Begin()
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
+
 	stmt, err := tx.Prepare(
 		"UPDATE domains SET class = ? WHERE name = ?",
 	)
@@ -123,25 +137,30 @@ func loadClasses(db *sql.DB, path string) error {
 		return err
 	}
 	defer stmt.Close()
+
 	for class, list := range m {
 		for _, name := range list {
 			if _, err := stmt.Exec(class, name); err != nil {
-				return err
+				return fmt.Errorf("set class tx: %w", err)
 			}
+
+			slog.Info("set class", "domain", name, "class", class)
 		}
 	}
 	return tx.Commit()
 }
 
 func listUnclassed(db *sql.DB, w io.Writer) error {
-	const q = "SELECT name FROM domains " +
-		"WHERE class IS NULL OR class = '' " +
-		"ORDER BY rank"
+	const q = `
+		SELECT name FROM domains
+		WHERE class IS NULL OR class = ''
+		ORDER BY rank`
 	rows, err := db.Query(q)
 	if err != nil {
-		return err
+		return fmt.Errorf("query: %w", err)
 	}
 	defer rows.Close()
+
 	for rows.Next() {
 		var name string
 		if err := rows.Scan(&name); err != nil {
@@ -149,5 +168,6 @@ func listUnclassed(db *sql.DB, w io.Writer) error {
 		}
 		fmt.Fprintln(w, name)
 	}
+
 	return rows.Err()
 }
